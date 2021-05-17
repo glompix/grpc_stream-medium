c1429157e69184b60678796e6a6469df9a5143a2
Samantha Branham <samantha@branh.am>
2021-05-16 21:45:39 -0700

    compose fix for macs; git version update; grpc devtools

--- js-client/src/App.js
+++ js-client/src/App.js
@@ -1,32 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import { SensorRequest  } from "./sensorpb/sensor_pb"
-import { SensorClient} from "./sensorpb/sensor_grpc_web_pb"
+import { SensorRequest } from "./sensorpb/sensor_pb"
+import { SensorClient } from "./sensorpb/sensor_grpc_web_pb"
 
 var client = new SensorClient('http://localhost:8000')
+const enableDevTools = window.__GRPCWEB_DEVTOOLS__ || (() => { });
+enableDevTools([
+  client,
+]);
+
 function App() {
   const [temp, setTemp] = useState(-9999);
-  const [humidity , setHumidity] = useState(-99999)
-  const [backColorTemp , setBackgroundColorTemp] = useState('#90BE6D')
-  const [backColorHum , setBackgroundColorHum] = useState('#90BE6D')
+  const [humidity, setHumidity] = useState(-99999)
+  const [backColorTemp, setBackgroundColorTemp] = useState('#90BE6D')
+  const [backColorHum, setBackgroundColorHum] = useState('#90BE6D')
 
   const getTemp = () => {
     console.log("called")
 
     var sensorRequest = new SensorRequest()
-    var stream = client.tempSensor(sensorRequest,{})
+    var stream = client.tempSensor(sensorRequest, {})
 
-    stream.on('data', function(response){
-        setTemp(response.getValue())
+    stream.on('data', function (response) {
+      setTemp(response.getValue())
     });
   };
 
   const getHumidity = () => {
     var sensorRequest = new SensorRequest()
-    var stream = client.humiditySensor(sensorRequest,{})
+    var stream = client.humiditySensor(sensorRequest, {})
 
-    stream.on('data',function(response){
+    stream.on('data', function (response) {
       setHumidity(response.getValue())
     })
   }
@@ -34,7 +39,7 @@ function App() {
   const detectTempAlert = () => {
     if (temp > 90 || temp < 30) {
       setBackgroundColorTemp('#F94144')
-    }else{
+    } else {
       setBackgroundColorTemp('#90BE6D')
     }
   }
@@ -43,43 +48,43 @@ function App() {
   const detectHumAlert = () => {
     if (humidity > 80) {
       setBackgroundColorHum('#F94144')
-    }else{
+    } else {
       setBackgroundColorHum('#90BE6D')
     }
   }
 
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     detectHumAlert()
   })
 
-  useEffect(()=>{
+  useEffect(() => {
     detectTempAlert()
   })
 
-  useEffect(()=>{
+  useEffect(() => {
     getTemp()
-  },[]);
+  }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     getHumidity()
-  },[]);
+  }, []);
 
   return (
     <div className="temp-cont">
-      <div className="temp" style={{backgroundColor:backColorTemp }}>
-          <h1> Temperature </h1>
-            
-          <h1> {temp} F </h1>
+      <div className="temp" style={{ backgroundColor: backColorTemp }}>
+        <h1> Temperature </h1>
+
+        <h1> {temp} F </h1>
       </div>
 
-      <div className="hum" style={{backgroundColor:backColorHum }}>
-          <h1> Humidity </h1>
-          <h1>{humidity} %</h1>
+      <div className="hum" style={{ backgroundColor: backColorHum }}>
+        <h1> Humidity </h1>
+        <h1>{humidity} %</h1>
       </div>
     </div>
- 
-  
+
+
   );
 }
 
